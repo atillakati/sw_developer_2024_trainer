875e8ab1d74426c5524e2a6a13a6a6f707952592
Atilla Kati (WIFI) <atilla@kati.bz>
2024-09-24 21:36:51 +0200

    Einführungsbeispiel Hello World C# Grundlagen
    Einführungsbeispiel Hello World C# Grundlagen

---
+++ src/20240924/helloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+	public class Application
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
+}
