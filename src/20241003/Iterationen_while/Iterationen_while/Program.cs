63b9d597cb7c99011c8f5827668460fdca749d7c
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-03 18:51:59 +0200

    Iterationen Grundlagen (for, while)

---
+++ src/20241003/Iterationen_while/Iterationen_while/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Iterationen_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string eingabe = string.Empty;
+
+            Console.WriteLine($"Time: {DateTime.Now.Second}");
+
+            while (DateTime.Now.Second > 30 && DateTime.Now.Second < 35)
+            {
+                Console.WriteLine($"Time: {DateTime.Now.Second}");
+            }
+
+            do
+            {
+                Console.Write("Geben Sie was ein: ");
+                eingabe = Console.ReadLine();
+            }
+            while (eingabe.Length < 5);
+        }
+    }
+}
