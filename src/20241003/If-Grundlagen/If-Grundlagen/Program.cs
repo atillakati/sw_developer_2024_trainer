8a35fb2a2449febf2c4f86e2d0620ea3e0b330a3
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-03 18:10:25 +0200

    if-else Grundlagen

---
+++ src/20241003/If-Grundlagen/If-Grundlagen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace If_Grundlagen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl = 2;
+
+            if(zahl < 5 && zahl > 0)
+            {
+                Console.WriteLine("Diese Zahl ist kleiner 5 und positiv.");
+            }
+            else
+            {
+                Console.WriteLine("Leider ist die Zahl nicht kleiner als 5.");
+            }
+        }
+    }
+}
