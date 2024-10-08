64c5e0125b57f7b0878108c92343196f86548ec8
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-08 20:01:26 +0200

    Grundlagen Array

---
+++ src/20241008/ArrayGL/ArrayGL/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ArrayGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constanten
+            const int MAX_ELEMENT_COUNT = 15;
+
+            //Deklaration
+            string[] listOfNames;
+
+            //Dimensionierung            
+            listOfNames = new string[MAX_ELEMENT_COUNT];
+            //listOfNames = new string[20];
+
+            //Initialisierung            
+            listOfNames[0] = string.Empty;
+            listOfNames[1] = string.Empty;
+            listOfNames[2] = string.Empty;
+            listOfNames[3] = string.Empty;
+
+            //Initialisierung "automatisch"
+            for(int i=0; i< listOfNames.Length; i++)
+            {
+                listOfNames[i] = "Test" + i;
+            }
+
+            //Ausgabe der Werte
+            for(int i = 0; i < listOfNames.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. Element Index: {i} Wert: {listOfNames[i]}");
+            }
+        }
+    }
+}
