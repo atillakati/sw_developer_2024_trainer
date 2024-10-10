1a6e63d5d6ef1611959742385c87e4a978dfbff5
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-10 21:39:15 +0200

    Grundlagen Klassen
    Grundlagen Klassen

---
+++ src/20241010/Klassen_GL/Klassen_GL/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Klassen_GL
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int CountOfPages;
+        public DateTime ReleaseDate;
+        public decimal Price;
+        public string Isbn;
+    }
+}
