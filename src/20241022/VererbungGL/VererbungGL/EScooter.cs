50c99f42c850a169de7e2a4812148f3d8ca305b9
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-22 21:32:05 +0200

    Vererbung klassisch & Enum

---
+++ src/20241022/VererbungGL/VererbungGL/EScooter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VererbungGL
+{
+    public class EScooter : Vehicle
+    {
+        private int _range;
+
+        public EScooter(string description, int range, ConsoleColor color) 
+            :base(description, 30, DateTime.Now, color)
+        {
+            _range = range;
+        }
+
+        public int Range { get => _range; }
+    }
+}
---
+++ src/20241022/VererbungGL/VererbungGL/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VererbungGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicleList = new Vehicle[]
+            {
+                new Car("Badmobil V12 Black Edition", 350, new DateTime(1990,5,7), FuelType.Benzin_98o),
+                new EScooter("Jumper Scooter light", 50, ConsoleColor.White),
+                new Car("BMW i3",195, new DateTime(2022, 9,2), FuelType.Elektrisch)
+            };
+
+            foreach(var v in vehicleList)
+            {
+                ShowVehicle(v);
+            }
+        }
+
+        private static void ShowVehicle(Vehicle vehicleToShow)
+        {
+            vehicleToShow.DisplayInfo();
+            Console.WriteLine();
+        }
+    }
+}
