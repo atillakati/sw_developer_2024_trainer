b9690aa074c289f1420b347f1065f1703eadc243
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-24 21:26:11 +0200

    Polymorphie Grundlagen virtual / override

---
+++ src/20241024/Polymorphie_GL/Polymorphie_GL/VehicleTypes/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Polymorphie_GL.VehicleTypes
+{
+    public class Car : Vehicle
+    {
+        private FuelType _fuel;
+        private int _seatCount;
+
+        public Car(string description, int maxSpeed, DateTime dateOfManufacture, FuelType fuel)
+            : base(description, maxSpeed, dateOfManufacture, ConsoleColor.Yellow)
+        {
+            _seatCount = 5;
+            _fuel = fuel;
+
+            ////nicht effizient!!!!
+            //Color = ConsoleColor.Yellow;
+        }
+
+
+        public FuelType Fuel
+        {
+            get { return _fuel; }
+        }
+
+        public int SeatCount { get => _seatCount; set => _seatCount = value; }
+
+        public override void DisplayInfo()
+        {
+            Console.ForegroundColor = Color;
+
+            Console.Write($"{Description}: {CurrentSpeed} / {MaxSpeed} km/h ");
+            Console.WriteLine($"Treibstoff: {_fuel} Sitzplätze: {_seatCount}");
+
+            Console.ResetColor();                        
+        }
+    }
+}
