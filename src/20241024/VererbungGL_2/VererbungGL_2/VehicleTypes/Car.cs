04ffe525b5828fcfb28c8d73ffedab36f5589bc8
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-24 21:26:30 +0200

    Vererbung: Hat-Ein Beziehung

---
+++ src/20241024/VererbungGL_2/VererbungGL_2/VehicleTypes/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VererbungGL_2.VehicleTypes
+{
+    public class Car : Vehicle
+    {
+        private FuelType _fuel;
+        private int _seatCount;
+
+        public Car(string description, int maxSpeed, DateTime dateOfManufacture, FuelType fuel)
+            : base(description, maxSpeed, dateOfManufacture, ConsoleColor.Yellow)
+        {
+            _seatCount = 5;
+            _fuel = fuel;
+
+            ////nicht effizient!!!!
+            //Color = ConsoleColor.Yellow;
+        }
+
+        public FuelType Fuel
+        {
+            get { return _fuel; }
+        }
+
+        public int SeatCount { get => _seatCount; set => _seatCount = value; }
+    }
+}
