58b5fab53e12441edd1e915d4dc147963c976a49
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-29 21:37:35 +0100

    Polymorphie 2 = abstract

---
+++ src/20241029/Polymorphy_II/Polymorphy_II/Program.cs
@@ -0,0 +1,31 @@
+﻿using Polymorphie_GL.VehicleTypes;
+using System;
+
+namespace Polymorphy_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicleList = new Vehicle[]
+            {
+                //new Vehicle("Ein Fahrzeug", 80, DateTime.Now, ConsoleColor.Red),
+                new Car("Super Car V6i", 250, DateTime.Now, FuelType.Diesel),
+                new EScooter("Jumper Super Light", 50, ConsoleColor.Cyan)
+            };
+
+            foreach (var vehicle in vehicleList)
+            {
+                vehicle.DisplayInfo();
+            }
+        }
+
+        private static void DoSomething(Vehicle vehicle)
+        {
+            vehicle.SetMediaPlayerState(true);
+            vehicle.SpeedUp(50);
+
+            vehicle.DisplayInfo();
+        }
+    }
+}
