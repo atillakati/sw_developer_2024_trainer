c89e49b97e6f111bc433503f9da0538ba36023dc
Atilla Kati (Company) <atilla.kati@solve.ch>
2024-10-31 07:39:21 +0100

    Copy of TeilnehmerVerwaltungV4

---
+++ src/20241031/Wifi.TeilnehmerVerwaltungV4/Wifi.TeilnehmerVerwaltungV4.DemoDataProvider/Entities/Coordinates.cs
@@ -0,0 +1,9 @@
+﻿namespace Wifi.TeilnehmerVerwaltungV4.DemoDataProvider.Entities
+{
+    public class Coordinates
+    {        
+        public string latitude { get; set; }
+
+        public string longitude { get; set; }
+    }
+}
---
+++ src/20241031/Wifi.TeilnehmerVerwaltungV4/Wifi.TeilnehmerVerwaltungV4.DemoDataProvider/Entities/RandomData.cs
@@ -0,0 +1,8 @@
+﻿namespace Wifi.TeilnehmerVerwaltungV4.DemoDataProvider.Entities
+{
+    public class RandomData
+    {
+        public Person[] results { get; set; }
+        public Info info { get; set; }
+    }
+}
