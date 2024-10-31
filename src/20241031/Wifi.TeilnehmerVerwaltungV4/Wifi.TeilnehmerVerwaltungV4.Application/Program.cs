dafb7636b4c33d3ba2ec3a63f004346e8f20a814
Atilla Kati (WIFI) <atilla@kati.bz>
2024-10-31 21:39:07 +0100

    Console Grid nuget
    Console Grid nuget

--- src/20241031/Wifi.TeilnehmerVerwaltungV4/Wifi.TeilnehmerVerwaltungV4.Application/Program.cs
+++ src/20241031/Wifi.TeilnehmerVerwaltungV4/Wifi.TeilnehmerVerwaltungV4.Application/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleTableExt;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,19 +7,30 @@ using System.Threading.Tasks;
 using Wifi.TeilnehmerVerwaltungV4.Core;
 using Wifi.TeilnehmerVerwaltungV4.DemoDataProvider;
 using Wifi.TeilnehmerVerwaltungV4.DemoDataProvider.Entities;
+using Wifi.TeilnehmerVerwaltungV4.Repository.Entities;
 
 namespace Wifi.TeilnehmerVerwaltungV4.Application
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {            
             var demoData = new RandomTeilnehmerProvider();
-
-            var data = demoData.GetRandomData(2000);
+            var data = demoData.GetRandomData(20);
 
             var geoPositionProvider = new GeoPositionProvider();
             var geoPositions = geoPositionProvider.GetRandomData(10);
+            
+
+            ConsoleTableBuilder
+               .From(data.ToList())
+               .WithFormat(ConsoleTableBuilderFormat.Default)
+               .ExportAndWriteLine(TableAligntment.Left);
+
+            ConsoleTableBuilder
+                .From(geoPositions.ToList())
+                .WithFormat(ConsoleTableBuilderFormat.Default)
+                .ExportAndWriteLine(TableAligntment.Left);
         }
 
         
