637c46f98c0c8ac05714e0b0272022103719c579
Atilla Kati (WIFI) <atilla@kati.bz>
2024-11-07 21:29:13 +0100

    First implementation of Playlist class, Unit-Tests, some interfaces

---
+++ src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Core.Test/PlaylistTests.cs
@@ -0,0 +1,112 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.PlaylistEditor.Core.Test
+{
+    [TestFixture]
+    public class PlaylistTests
+    {
+        private Playlist _fixture;
+        private Mock<IPlaylistItem> _mockedItem;
+
+        [SetUp]
+        public void Init()
+        {
+            _fixture = new Playlist("Demo Playlist", "Testing");
+
+            //setup mocked item
+            _mockedItem = new Mock<IPlaylistItem>();
+            _mockedItem.Setup(x => x.Duration).Returns(TimeSpan.FromSeconds(53));
+        }
+
+        [Test]
+        public void Duration_get_NoItems()
+        {
+            var result = _fixture.Duration;
+
+            Assert.That(result, Is.EqualTo(TimeSpan.Zero));
+        }
+
+        [Test]
+        public void Duration_get()
+        {
+            //arrange
+            _fixture.Add(_mockedItem.Object);
+            _fixture.Add(_mockedItem.Object);
+
+            //act
+            var result = _fixture.Duration;
+
+            //assert
+            Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(106)));
+        }
+
+        [Test]
+        public void Title_set()
+        {
+            //arrange
+            var newTitle = "Gandalf";            
+
+            //act
+            _fixture.Title = newTitle;
+            var result = _fixture.Title;
+
+            //assert
+            Assert.That(result, Is.EqualTo(newTitle));
+        }
+
+        [Test]
+        public void Title_get()
+        {
+            //arrange            
+
+            //act
+            var result = _fixture.Title;
+
+            //assert
+            Assert.That(result, Is.EqualTo("Demo Playlist"));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            //arrange
+            int erg = 0;
+
+            //act
+            erg = 5 + 6;
+
+            //assert
+            Assert.That(erg, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void Add()
+        {
+            //arrange
+
+            //act
+            _fixture.Add(_mockedItem.Object);
+
+            //assert
+            Assert.That(_fixture.Items.Any(), Is.True);
+        }
+
+        [Test]
+        public void Add_null()
+        {
+            //arrange
+
+            //act
+            _fixture.Add(null);
+
+            //assert
+            Assert.That(_fixture.Items.Any(), Is.False);
+        }
+    }
+}
---
+++ src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Core/Playlist.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.PlaylistEditor.Core
+{
+    public class Playlist
+    {
+		private string _title;
+		private string _author;
+		private DateTime _dateOfCreation;
+		private List<IPlaylistItem> _items;
+
+        public Playlist(string title, string author)
+			:this(title, author, DateTime.Now) { }
+
+        public Playlist(string title, string author, DateTime dateOfCreation)
+        {
+			_title = title;
+            _author = author;
+            _dateOfCreation = dateOfCreation;
+
+			_items = new List<IPlaylistItem>();
+        }
+
+        public IEnumerable<IPlaylistItem> Items
+		{
+			get { return _items; }			
+		}
+
+		public TimeSpan	Duration 
+		{
+			get
+			{
+				if (_items.Any())
+				{
+					var duration = TimeSpan.Zero;					
+					_items.ForEach(x => duration += x.Duration);
+
+					return duration;
+				}
+
+				return TimeSpan.Zero;
+			}
+		}
+
+		public DateTime DateOfCreation
+        {
+			get { return _dateOfCreation; }			
+		}
+
+		public string Author
+		{
+			get { return _author; }			
+		}
+
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+
+		public void Add(IPlaylistItem item)
+		{
+			if(item == null)
+			{
+				return;
+			}
+
+			_items.Add(item);
+		}
+
+		public void Remove(IPlaylistItem item)
+		{
+			_items.Remove(item);
+		}
+
+		public void Clear()
+		{
+			_items.Clear();
+		}
+	}
+}
