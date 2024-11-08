4126b8f7469e740f9caa84882d0d3beda4a7116a
Atilla Kati (Company) <atilla.kati@solve.ch>
2024-11-08 08:48:45 +0100

    Implement PictureItem and tests

---
+++ src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Repositories.Test/PictureItemTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using Wifi.PlaylistEditor.Core;
+
+namespace Wifi.PlaylistEditor.Repositories.Test
+{
+    [TestFixture]
+    public class PictureItemTests
+    {
+        private IPlaylistItem _fixture;
+
+        [SetUp]
+        public void Init()
+        {
+            _fixture = new PictureItem("Assets/Debugging.jpg");
+        }
+
+        [Test]
+        public void Title_get()
+        {
+            Assert.That(_fixture.Title, Is.EqualTo("Debugging"));
+        }
+
+        [Test]
+        public void Artist_get()
+        {
+            Assert.That(_fixture.Artist, Is.EqualTo("Unknown"));
+        }
+
+        [Test]
+        public void Duration_get()
+        {
+            Assert.That(_fixture.Duration, Is.EqualTo(TimeSpan.FromSeconds(10)));
+        }
+
+        [Test]
+        public void FilePath_get()
+        {
+            Assert.That(_fixture.FilePath, Is.EqualTo("Assets/Debugging.jpg"));
+        }
+
+        [Test]
+        public void Thumbnail_get()
+        {
+            Assert.That(_fixture.Thumbnail, Is.Not.Null);
+            Assert.That(_fixture.Thumbnail.Height, Is.EqualTo(69));
+            Assert.That(_fixture.Thumbnail.Width, Is.EqualTo(125));
+        }
+    }
+}
---
+++ src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Repositories/PictureItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wifi.PlaylistEditor.Core;
+
+namespace Wifi.PlaylistEditor.Repositories
+{
+    public class PictureItem : IPlaylistItem
+    {
+        private readonly string _filePath; 
+        private string _title;
+        private Image _thumbnail;
+
+        public PictureItem(string filePath)
+        {
+            _filePath = filePath;
+            _title = Path.GetFileNameWithoutExtension(_filePath);
+            _thumbnail = ScaleImage(Image.FromFile(_filePath), 125, 125);
+        }
+
+        private Bitmap ScaleImage(Image bmp, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / bmp.Width;
+            var ratioY = (double)maxHeight / bmp.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(bmp.Width * ratio);
+            var newHeight = (int)(bmp.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(bmp, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+
+        public string Title { get => _title; }
+        public string Artist { get => "Unknown"; }
+        public TimeSpan Duration { get => TimeSpan.FromSeconds(10); }
+        public string FilePath { get => _filePath; }
+        public Image Thumbnail { get => _thumbnail; }
+    }
+}
