905d50863e7119dd6a6356766469bb80bdd22077
Atilla Kati (Company) <atilla.kati@solve.ch>
2024-11-08 07:59:15 +0100

    Add Mp3Repository implementation with tests

---
+++ src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Repositories.Test/Mp3RepositoryTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wifi.PlaylistEditor.Core;
+
+namespace Wifi.PlaylistEditor.Repositories.Test
+{
+    [TestFixture]
+    public class Mp3RepositoryTests
+    {
+        private IPlaylistItem _fixture;
+
+        [SetUp]
+        public void Init()
+        {
+            _fixture = new Mp3Repository("DemoFile.mp3");
+        }
+
+        [Test]
+        public void Title_get()
+        {
+            Assert.That(_fixture.Title, Is.EqualTo("Grenade"));
+        }
+
+        [Test]
+        public void Artist_get()
+        {
+            Assert.That(_fixture.Artist, Is.EqualTo("Bruno Mars"));
+        }
+
+        [Test]
+        public void Duration_get()
+        {
+            Assert.That(_fixture.Duration, Is.EqualTo(TimeSpan.FromSeconds(222)));
+        }
+
+        [Test]
+        public void FilePath_get()
+        {
+            Assert.That(_fixture.FilePath, Is.EqualTo("DemoFile.mp3"));
+        }
+
+        [Test]
+        public void Thumbnail_get()
+        {
+            Assert.That(_fixture.Thumbnail, Is.Not.Null);
+        }
+    }
+}
---
+++ src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Repositories/Mp3Repository.cs
@@ -0,0 +1,47 @@
+﻿using Id3;
+using System;
+using System.Drawing;
+using System.IO;
+using Wifi.PlaylistEditor.Core;
+
+namespace Wifi.PlaylistEditor.Repositories
+{
+    public class Mp3Repository : IPlaylistItem
+    {
+        private string _title;
+        private string _artist;
+        private TimeSpan _duration;
+        private string _filePath;
+        private Image _thumbnail;
+
+
+        public Mp3Repository(string filePath)
+        {
+            var mp3 = new Mp3(filePath);
+            var id3Tag = mp3.GetTag(Id3TagFamily.Version2X);
+
+            _title = id3Tag.Title;
+            _artist = id3Tag.Artists;
+            _duration = mp3.Audio.Duration;
+            _filePath = filePath;
+            _thumbnail = GetThumbnail(id3Tag);
+        }
+
+        private Image GetThumbnail(Id3Tag id3Tag)
+        {
+            if (id3Tag.Pictures == null || id3Tag.Pictures.Count == 0)
+            {
+                return null;
+            }
+
+            _thumbnail = Image.FromStream(new MemoryStream(id3Tag.Pictures[0].PictureData));
+            return _thumbnail;
+        }
+
+        public string Title { get => _title; }
+        public string Artist { get => _artist; }
+        public TimeSpan Duration { get => _duration; }
+        public string FilePath { get => _filePath; }
+        public Image Thumbnail { get => _thumbnail; }
+    }
+}
