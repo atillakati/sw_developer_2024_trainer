973b42f323d1f76ea72d9d399ff43d0c7e637a07
Atilla Kati (Company) <atilla.kati@solve.ch>
2024-11-08 07:25:04 +0100

    Add unit tests for Playlist class
    Add unit tests for Playlist class

--- src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Core.Test/PlaylistTests.cs
+++ src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Core.Test/PlaylistTests.cs
@@ -13,6 +13,7 @@ namespace Wifi.PlaylistEditor.Core.Test
     {
         private Playlist _fixture;
         private Mock<IPlaylistItem> _mockedItem;
+        private Mock<IPlaylistItem> _mockedItem2;
 
         [SetUp]
         public void Init()
@@ -22,6 +23,11 @@ namespace Wifi.PlaylistEditor.Core.Test
             //setup mocked item
             _mockedItem = new Mock<IPlaylistItem>();
             _mockedItem.Setup(x => x.Duration).Returns(TimeSpan.FromSeconds(53));
+            _mockedItem.Setup(x => x.Title).Returns("Demo Title 1");
+
+            _mockedItem2 = new Mock<IPlaylistItem>();
+            _mockedItem2.Setup(x => x.Duration).Returns(TimeSpan.FromSeconds(23));
+            _mockedItem2.Setup(x => x.Title).Returns("Demo Title 2");
         }
 
         [Test]
@@ -37,13 +43,13 @@ namespace Wifi.PlaylistEditor.Core.Test
         {
             //arrange
             _fixture.Add(_mockedItem.Object);
-            _fixture.Add(_mockedItem.Object);
+            _fixture.Add(_mockedItem2.Object);
 
             //act
             var result = _fixture.Duration;
 
             //assert
-            Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(106)));
+            Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(76)));
         }
 
         [Test]
@@ -73,6 +79,32 @@ namespace Wifi.PlaylistEditor.Core.Test
         }
 
         [Test]
+        public void DateOfCreation_get()
+        {
+            //arrange            
+            var dateOfCreation = new DateTime(2024, 11, 8);
+            var fixture = new Playlist("Demo Playlist", "Testing", dateOfCreation);
+
+            //act
+            var result = fixture.DateOfCreation;
+
+            //assert
+            Assert.That(result, Is.EqualTo(dateOfCreation));
+        }
+
+        [Test]
+        public void Author_get()
+        {
+            //arrange            
+
+            //act
+            var result = _fixture.Author;
+
+            //assert
+            Assert.That(result, Is.EqualTo("Testing"));
+        }
+
+        [Test]
         public void FirstTest()
         {
             //arrange
@@ -83,6 +115,65 @@ namespace Wifi.PlaylistEditor.Core.Test
 
             //assert
             Assert.That(erg, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void Remove()
+        {
+            //arrange
+            _fixture.Add(_mockedItem.Object);
+            _fixture.Add(_mockedItem2.Object);
+
+            //act
+            _fixture.Remove(_mockedItem.Object);
+
+            //assert
+            Assert.That(_fixture.Items.Count, Is.EqualTo(1));
+            Assert.That(_fixture.Items.FirstOrDefault(), Is.Not.Null);
+            Assert.That(_fixture.Items.FirstOrDefault()?.Title, Is.EqualTo("Demo Title 2"));
+        }
+
+        [Test]
+        public void Remove_null()
+        {
+            //arrange
+            _fixture.Add(_mockedItem.Object);
+            _fixture.Add(_mockedItem2.Object);
+
+            //act
+            _fixture.Remove(null);
+
+            //assert
+            Assert.That(_fixture.Items.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Remove_NotExistingItem()
+        {
+            //arrange
+            _fixture.Add(_mockedItem.Object);
+
+            //act
+            _fixture.Remove(_mockedItem2.Object);
+
+            //assert
+            Assert.That(_fixture.Items.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Clear()
+        {
+            //arrange
+            _fixture.Add(_mockedItem.Object);
+            _fixture.Add(_mockedItem2.Object);
+            var itemCount = _fixture.Items.Count();
+
+            //act
+            _fixture.Clear();
+
+            //assert
+            Assert.That(itemCount, Is.EqualTo(2));
+            Assert.That(_fixture.Items.Any(), Is.False);
         }
 
         [Test]
