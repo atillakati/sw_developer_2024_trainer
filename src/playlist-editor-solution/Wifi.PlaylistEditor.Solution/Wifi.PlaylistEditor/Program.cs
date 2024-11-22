765c116a4d665362d654c8b15f5f40aa5679b0a9
Atilla Kati (Company) <atilla.kati@solve.ch>
2024-11-22 07:49:09 +0100

    Implement dynamic repository factory using AutoFac nuget

--- src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor/Program.cs
+++ src/playlist-editor-solution/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Autofac;
 using System;
-using System.Collections.Generic;
-using System.Deployment.Application;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 using Wifi.PlaylistEditor.CommonTypes;
 using Wifi.PlaylistEditor.Core;
@@ -23,17 +21,35 @@ namespace Wifi.PlaylistEditor
 
             builder.RegisterType<frm_CreateNewPlaylist>().As<ICreatePlaylist>();
             builder.RegisterType<PlaylistFactory>().As<IPlaylistFactory>();
-            //builder.RegisterType<RepositoryFactory>().As<IRepositoryFactory>();            
+            builder.RegisterType<DynamicRepositoryFactory>().As<IRepositoryFactory>();            
             builder.RegisterType<PlaylistItemFactory>().As<IPlaylistItemFactory>();
             builder.RegisterType<frm_main>();
 
+            RegisterFromAssemblies(builder);
+
             var container = builder.Build();
 
-            //var repoFactory = container.Resolve<IRepositoryFactory>();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(container.Resolve<frm_main>());
+        }
+
+        private static void RegisterFromAssemblies(ContainerBuilder builder)
+        {
+            var filePath = Assembly.GetExecutingAssembly().Location;
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetDirectoryName(filePath));
+            var files = directoryInfo.EnumerateFiles("Wifi.*.dll");
+            
+            foreach (var assemblyPath in files)
+            {
+                var dataAccess = Assembly.LoadFile(assemblyPath.FullName);
+
+                builder.RegisterAssemblyTypes(dataAccess)
+                    .Where(t => t.Name.EndsWith("Repository"))
+                    .AsImplementedInterfaces();
+            }
         }
     }
 }
