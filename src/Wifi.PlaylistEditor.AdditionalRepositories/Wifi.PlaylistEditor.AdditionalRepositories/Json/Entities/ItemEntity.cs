61928b89fc2c3c8db3bc7925bad0e4d4ab5f806d
Atilla Kati (WIFI) <atilla@kati.bz>
2024-11-26 19:13:19 +0100

    Implement JsonRepository (as Add-IN)

---
+++ src/Wifi.PlaylistEditor.AdditionalRepositories/Wifi.PlaylistEditor.AdditionalRepositories/Json/Entities/ItemEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Wifi.PlaylistEditor.AdditionalRepositories.Json.Entities
+{
+    internal class ItemEntity
+    {
+        public string Path { get; set; }
+        public string Duration { get; set; }
+    }
+
+}
